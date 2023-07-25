71fc8f4ceea2b1757244aa26577028e1c7042ddd
Nishant Mehta <nishme0708@gmail.com>
2023-07-25 05:33:37 +0530

    Fix class typo

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ function Loader() {
     <div role='status'>
       <svg
         aria-hidden='true'
-        class='w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+        className='w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
@@ -17,7 +17,7 @@ function Loader() {
           fill='currentFill'
         />
       </svg>
-      <span class='sr-only'>Loading...</span>
+      <span className='sr-only'>Loading...</span>
     </div>
   );
 }
